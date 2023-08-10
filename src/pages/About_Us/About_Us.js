986bcd095c5409da7d2eb107e7df913df4299451
Mohsin <heykhattak@gmail.com>
2023-08-10 06:50:52 +0500

    Added 3 more section to about us

--- src/pages/About_Us/About_Us.js
+++ src/pages/About_Us/About_Us.js
@@ -11,52 +11,128 @@ function AboutUs() {
         </div>
       </div>
       <div className="below_main">
+        <div className="below_main_left">
+          <h1 className="heading">
+            <span className="heading_light">About</span> DPFZE
+          </h1>
+          <hr className="horizontal_line horizontal_line_blue" />
+          <p className="paragraph">
+            Dynamic Products FZE is a supplier of steel and alloy piping
+            materials, Valves and accessories. Comprised of a skilled and
+            knowledgeable team with over 25 years of industry experience, we are
+            ready to provide solutions for all your piping material
+            requirements. We are your specialist when it comes to ambitious and
+            challenging projects.
+            <br />
+            <br /> Our main scope of business is to supply
+            <strong> Pipes, Fittings, Flanges, Valves, Plates/Sheets </strong>,
+            and other Industrial Parts, all the while continuously trying to set
+            ourselves apart with unparalleled customer service and an abundant
+            resources of material grades, so that no part you need is too hard
+            to find.
+          </p>
+        </div>
+        <div className="below_main_right ">
+          <img
+            className="image_style"
+            src="https://i.ebayimg.com/images/g/92sAAOSwOi5gxmyU/s-l1600.jpg"
+            alt="some pipes"
+          />
+        </div>
+      </div>
+      <div className="below_main third_section">
+        <div className="below_main_left">
+          <img
+            className="image_style"
+            src="https://saurinenterprise.in/img/service/PVC-Pipes.jpg"
+            alt="some pipes"
+          />
+        </div>
+        <div className="below_main_right">
+          <h1 className="heading">
+            <span className="heading_light">DPFZE </span> Expertise
+          </h1>
+          <hr className="horizontal_line horizontal_line_blue" />
+          <p className="paragraph">
+            <strong>
+              Project Urgent Materials / Shutdown / Emergency Requirement
+            </strong>
+            <br />
+            Providing express delivery lead time for Hard-to-Find Material
+            Grades & Sizes <br />
+            from ex-stock or quick manufacturing is our expertise. We are
+            connected with
+            <br /> more than hundred manufacturers and stockholders and market
+            knowhow
+            <br /> makes this possible to meet the dead lines on shutdowns and
+            urgent material demands on site. <br /> Lorem Ipsum is simply dummy
+            text of the printing and typesetting industry. Lorem Ipsum has been
+            the industry's standard dummy text ever since the 1500s, when an
+            unknown printer took a galley of type and scrambled it to make a
+            type specimen book. It has survived not only five centuries, but
+            also the leap into electronic typesetting, remaining essentially
+            unchanged.
+          </p>
+        </div>
+      </div>
+      <div className="fourth_section">
         <h1 className="heading">
-          <span className="heading_light">About</span> DPFZE
+          <span className="heading_light">What DPFZE</span> offers?
         </h1>
         <hr className="horizontal_line horizontal_line_blue" />
         <p className="paragraph">
-          Dynamic Products FZE is a supplier of steel and alloy piping
-          materials, Valves and accessories. Comprised of a skilled and
-          knowledgeable team with over 25 years of industry experience, we are
-          ready to provide solutions for all your piping material requirements.
-          We are your specialist when it comes to ambitious and challenging
-          projects.
-          <br />
-          <br /> Our main scope of business is to supply Pipes, Fittings,
-          Flanges, Valves, Plates/Sheets, and other Industrial Parts, all the
-          while continuously trying to set ourselves apart with unparalleled
-          customer service and an abundant resources of material grades, so that
-          no part you need is too hard to find.
+          We can supply complete packages with a mix of items, sizes and
+          material grades. This makes the procurement process at our client ends
+          easier and more competitive. All our products meet international
+          standards such as ASTM, ASME, ANSI, NACE, API, DIN etc. Exotic
+          material grades, uncommon & non-stock sizes and challenging delivery
+          lead times is our competitive edge.Lorem Ipsum is simply dummy text of
+          the printing and typesetting industry. Lorem Ipsum has been the
+          industry's standard dummy text ever since the 1500s, when an unknown
+          printer took a galley of type and scrambled it to make a type specimen
+          book. It has survived not only five centuries, but also the leap into
+          electronic typesetting, remaining essentially unchanged. It was
+          popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop
+          publishing software like Aldus PageMaker including versions of Lorem
+          Ipsum.
         </p>
       </div>
-      <div className="who_are_we">
-        <div className="who_are_we_left">
-          <h1 className="about_us_title">30+</h1>
-          <h3>Years Of Experience</h3>
-          <h1 className="about_us_title">200k</h1>
-          <h3>Tons of Annual TurnOver </h3>
-          <h1 className="about_us_title">5</h1>
-          <h3>GCC Countries</h3>
+      <div className="fifth_section">
+        <div className="counter_div">
+          <h3 className="numbers_count">100+</h3>
+          <p className="numbers_count_below">Customers</p>
         </div>
-        <div className="who_are_we_right">
-          <h1 className="heading">
-            <span className="heading_light">
-              Who <br />
-            </span>
-            are we
-          </h1>
-          <hr className=" horizontal_line horizontal_line_blue" />
-          <p className="paragraph">
-            We are your specialist when it comes to ambitious and challenging
-            projects. Our main scope of business is to supply Pipes, Fittings,
-            Flanges, Valves, Plates/Sheets, and other Industrial Parts <br />
-            <br />
-            All that while continuously trying to set ourselves apart with
-            unparalleled customer service and an abundant resources of material
-            grades, so that no part you need is too hard to find.
-          </p>
+        <div className="counter_div">
+          <h3 className="numbers_count">80+</h3>
+          <p className="numbers_count_below">Projects Done</p>
         </div>
+        <div className="counter_div">
+          <h3 className="numbers_count">90000+</h3>
+          <p className="numbers_count_below">Material Supplied</p>
+        </div>
+      </div>
+      <div className="fourth_section">
+        <h1 className="heading">
+          <span className="heading_light">We focus Client's</span> Approved
+          Manufacturers!
+        </h1>
+        <hr className="horizontal_line horizontal_line_blue" />
+        <p className="paragraph">
+          Selection of Manufacturer and Origin play an important part in the
+          quality of a product and materials. We strictly adhere to The Client
+          Approved Manufacturer’s List and Country of Origin. All material
+          supplied is from well-reputed manufacturers. Lorem Ipsum is simply
+          dummy text of the printing and typesetting industry. Lorem Ipsum has
+          been the industry's standard dummy text ever since the 1500s, when an
+          unknown printer took a galley of type and scrambled it to make a type
+          specimen book. It has survived not only five centuries, but also the
+          leap into electronic typesetting, remaining essentially unchanged. It
+          was popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop
+          publishing software like Aldus PageMaker including versions of Lorem
+          Ipsum.
+        </p>
       </div>
     </>
   );
