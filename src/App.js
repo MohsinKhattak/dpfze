5420921ea5f9c336305263b9a05b3005b1d96b8b
Mohsin <heykhattak@gmail.com>
2023-08-23 21:34:18 +0500

    Made Contact Us Responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import AboutUs from "./pages/About_Us/About_Us";
 import ContactUs from "./pages/Contact_Us/Contact_Us";
 function App() {
-  return <AboutUs></AboutUs>;
+  return <ContactUs></ContactUs>;
 }
 
 export default App;
