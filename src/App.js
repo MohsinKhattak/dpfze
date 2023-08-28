3554d22311328c20d478ede9cbd25c4c230ac222
yooSavage7 <maazharoon147@gmail.com>
2023-08-28 19:47:05 +0500

    Committing local changes before merge

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutUs from "./pages/About_Us/About_Us";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return <AboutUs></AboutUs>;
+  return(
+    <Router>
+  <Routes>
+    <Route path="/about" element={<AboutUs/>} />
+    <Route path="/" element={<Home/>} />
+  </Routes>
+</Router>
+
+  );
+  
+   
 }
 
 export default App;
